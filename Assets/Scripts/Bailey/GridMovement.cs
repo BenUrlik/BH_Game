dc4cfb50f05a14f0891934b4cfcf909d13c89d9a
JulianBaileySoovajian <julianbaileysoovajian@gmail.com>
2023-04-28 16:54:31 -0700

    Added a basic grid movement system

--- Assets/Scripts/Bailey/GridMovement.cs
+++ Assets/Scripts/Bailey/GridMovement.cs
@@ -1,18 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class GridMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform playerTransform;
+    public InputAction playerAction;
+    private float movementTimer = 0;
+    public float movementDelay = 2;
+    Vector2 moveDirection = Vector2.zero;
+
+    private void OnEnable()
     {
-        
+        playerAction.Enable();
+    }
+    private void OnDisable()
+    {
+        playerAction.Disable();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        moveDirection = playerAction.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        movementTimer -= Time.fixedDeltaTime;
+        if (movementTimer > 0)
+            return;
+            if (moveDirection.y < 0)
+            {
+                this.transform.Translate(0, -1, 0);
+            }
+            else if (moveDirection.y > 0)
+            {
+                this.transform.Translate(0, 1, 0);
+            }
+            else if (moveDirection.x > 0)
+            {
+                this.transform.Translate(1, 0, 0);
+            }
+            else if (moveDirection.x < 0)
+            {
+                this.transform.Translate(-1, 0, 0);
+            }
+        movementTimer = movementDelay;
     }
 }
