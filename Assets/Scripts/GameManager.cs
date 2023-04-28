6bee160d9a479637f655c9dd7a1846df6ace980a
BenUrlik <benurlik@gmail.com>
2023-04-28 16:22:35 -0500

    Updated the GameManager script
    Added the game states.
    Additionally added the TitleScreen scene and a bullet prefab

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public static GameManager Instance;
+    public GameState State;
+    public static event Action<GameState> OnGameStateChanged;
+
+    void Awake() {
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void UpdateGameState(GameState newState) {
+        State = newState;
+
+        switch(newState) {
+            case GameState.StartGame:
+                break;
+            case GameState.Death:
+                break;
+            case GameState.Pause:
+                break;
+            case GameState.Victory:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+        }
+
+        OnGameStateChanged?.Invoke(newState);
+    }
+
+    public enum GameState {
+        StartGame,
+        Death,
+        Pause,
+        Victory
     }
 }
