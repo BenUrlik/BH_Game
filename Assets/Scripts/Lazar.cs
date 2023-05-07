3060794e7024c6b33e13638016bf977fd8930938
BenUrlik <benurlik@gmail.com>
2023-05-06 21:30:31 -0500

    Added a Universal Rendering Pipeline to the game

--- Assets/Scripts/Lazar.cs
+++ Assets/Scripts/Lazar.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class Lazar : MonoBehaviour
 {
-    private LineRenderer lr;
+    [SerializeField] private float distanceRay = 100;
+    public Transform laserFirePoint;
+    public LineRenderer lr;
+
     void Start() {
         lr = GetComponent<LineRenderer>();
     }
 
     void Update() {
-        lr.SetPosition(0, transform.position);
-        RaycastHit hit;
-        if(Physics.Raycast(transform.position, transform.forward, out hit)) {
-            if(hit.collider) {
-                Debug.Log("Hit!");
-            }
+        ShootLaser();
+    }
+
+    void ShootLaser() {
+        if(Physics2D.Raycast(transform.position, transform.right)) {
+            RaycastHit2D hit = Physics2D.Raycast(laserFirePoint.position, transform.right);
+            Draw2DRay(laserFirePoint.position, hit.point);
+            Debug.Log("Hit!");
         }
-        else lr.SetPosition(1, transform.forward);
+        else Draw2DRay(laserFirePoint.position, laserFirePoint.transform.right * distanceRay);
+    }
+
+    void Draw2DRay(Vector2 startPos, Vector2 endPos) {
+        lr.SetPosition(0, startPos);
+        lr.SetPosition(1, endPos);
     }
 }
